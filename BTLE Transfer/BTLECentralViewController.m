8e988483e7f190376fcb2400df11a3c25f67a941
Lorenz Hänggi <lolo8304@gmail.com>
2015-09-10 23:42:39 +0200

    refactoring receiver ...

--- BTLE Transfer/BTLECentralViewController.m
+++ BTLE Transfer/BTLECentralViewController.m
@@ -51,25 +51,21 @@
 
 #import "BTLECentralViewController.h"
 #import <CoreBluetooth/CoreBluetooth.h>
-
 #import "TransferService.h"
+#import "BTLECentral.h"
 
-@interface BTLECentralViewController () <CBCentralManagerDelegate, CBPeripheralDelegate>
+@interface BTLECentralViewController () <ReceiverDelegate>
 
 @property (strong, nonatomic) IBOutlet UITextView   *textview;
 @property (weak, nonatomic) IBOutlet UILabel *dbStrength;
-
-@property (strong, nonatomic) CBCentralManager      *centralManager;
-@property (strong, nonatomic) CBPeripheral          *discoveredPeripheral;
-@property (strong, nonatomic) NSMutableData         *data;
 
 @property (weak, nonatomic) IBOutlet UISegmentedControl *channel;
 @property (weak, nonatomic) IBOutlet UILabel *channelText;
 @property (weak, nonatomic) IBOutlet UISwitch *connected;
 
+@property (strong, nonatomic) BTLECentral *central;
 
 - (IBAction)channelChanged:(UISegmentedControl *)sender;
-
 
 @end
 
@@ -81,25 +77,13 @@
 
 #pragma mark - View Lifecycle
 
-- (void) startBluetooth {
-    // Start up the CBCentralManager
-    _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
-    
-    // And somewhere to store the incoming data
-    _data = [[NSMutableData alloc] init];
-}
-
-- (void) stopBluetooth {
-    // Don't keep it going while we're not showing.
-    [self.centralManager stopScan];
-    [self cleanup];
-    NSLog(@"Scanning stopped");
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self startBluetooth];
+    self.central = [[BTLECentral alloc] initWith: self];
+    
+    [self.central startBluetooth];
     [self channelChangedToInteger: 0];
     [[self connected] setOn: false];
 }
@@ -116,266 +100,52 @@
 - (IBAction)channelChanged:(UISegmentedControl *)sender {
     
     [self channelChangedToInteger: sender.selectedSegmentIndex];
-    [self stopBluetooth];
-    [self startBluetooth];
+    [self.central stopBluetooth];
+    [self.central startBluetooth];
 }
+
 - (IBAction)channelChangedToInteger:(NSInteger *)sender {
-    
     [TransferService setValue: sender];
     self.channelText.text = [TransferService getTRANSFER_SERVICE_UUID];
 }
 
 
+
+
+
+
 #pragma mark - Central Methods
 
-
-
-/** centralManagerDidUpdateState is a required protocol method.
- *  Usually, you'd check for other states to make sure the current device supports LE, is powered on, etc.
- *  In this instance, we're just using it to wait for CBCentralManagerStatePoweredOn, which indicates
- *  the Central is ready to be used.
- */
-- (void)centralManagerDidUpdateState:(CBCentralManager *)central
-{
-    if (central.state != CBCentralManagerStatePoweredOn) {
-        // In a real app, you'd deal with all the states correctly
-        return;
-    }
-    
-    // The state must be CBCentralManagerStatePoweredOn...
-
-    // ... so start scanning
-    [self scan];
-    
-}
-
-
-/** Scan for peripherals - specifically for our service's 128bit CBUUID
- */
-- (void)scan
-{
-    [self.centralManager scanForPeripheralsWithServices:@[[TransferService getTRANSFER_SERVICE_CBUUID]]
-                                                options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
-    
-    NSLog(@"Scanning started");
-}
-
-
-/** This callback comes whenever a peripheral that is advertising the TRANSFER_SERVICE_UUID is discovered.
- *  We check the RSSI, to make sure it's close enough that we're interested in it, and if it is, 
- *  we start the connection process
- */
-- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI
-{
+- (BOOL)isRSSIAllowed: (int) RSSI {
     // Reject any where the value is above reasonable range
-    if (RSSI.integerValue > -15) {
-        return;
+    if (RSSI > -15) {
+        return false;
     }
-
+    
     // Reject if the signal strength is too low to be close enough (Close is around -22dB)
-    if (RSSI.integerValue < -270) {
-        return;
+    if (RSSI < -2700) {
+        return false;
     }
-    self.dbStrength.text = [NSString stringWithFormat: @"%ld", (long)RSSI.integerValue];
-    
-    //NSLog(@"Discovered %@ at %@", peripheral.name, RSSI);
-    
-    // Ok, it's in range - have we already seen it?
-    if (self.discoveredPeripheral != peripheral) {
-        
-        // Save a local copy of the peripheral, so CoreBluetooth doesn't get rid of it
-        self.discoveredPeripheral = peripheral;
-        
-        // And connect
-        //NSLog(@"Connecting to peripheral %@", peripheral);
-        [self.centralManager connectPeripheral:peripheral options:nil];
-    }
+    return true;
 }
 
 
-/** If the connection fails for whatever reason, we need to deal with it.
- */
-- (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
-{
-    NSLog(@"Failed to connect to %@. (%@)", peripheral, [error localizedDescription]);
-    [self cleanup];
+- (void) strengthRSSI:(int)RSSI {
+    self.dbStrength.text = [NSString stringWithFormat: @"%ld", (long)RSSI];
 }
 
 
-/** We've connected to the peripheral, now we need to discover the services and characteristics to find the 'transfer' characteristic.
- */
-- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
-{
-    NSLog(@"Peripheral Connected");
-    
-    // Stop scanning
-    [self.centralManager stopScan];
-    NSLog(@"Scanning stopped");
-    
-    // Clear the data that we may already have
-    [self.data setLength:0];
-
-    // Make sure we get the discovery callbacks
-    peripheral.delegate = self;
-    
-    // Search only for services that match our UUID
-    [peripheral discoverServices:@[[TransferService getTRANSFER_SERVICE_CBUUID]]];
+- (void) dataReceived:(NSData *)data {
+    NSString *dataString = [[NSString alloc] initWithData: data encoding:NSUTF8StringEncoding];
+    [self.textview setText: dataString];
 }
 
 
-/** The Transfer Service was discovered
- */
-- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
-{
-    if (error) {
-        NSLog(@"Error discovering services: %@", [error localizedDescription]);
-        [self cleanup];
-        return;
-    }
-    
-    // Discover the characteristic we want...
-    
-    // Loop through the newly filled peripheral.services array, just in case there's more than one.
-    for (CBService *service in peripheral.services) {
-        [peripheral discoverCharacteristics:@[[CBUUID UUIDWithString: TRANSFER_CHARACTERISTIC_UUID]] forService:service];
-    }
-}
-
-
-/** The Transfer characteristic was discovered.
- *  Once this has been found, we want to subscribe to it, which lets the peripheral know we want the data it contains
- */
-- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
-{
-    // Deal with errors (if any)
-    if (error) {
-        NSLog(@"Error discovering characteristics: %@", [error localizedDescription]);
-        [self cleanup];
-        return;
-    }
-    
-    // Again, we loop through the array, just in case.
-    for (CBCharacteristic *characteristic in service.characteristics) {
-        
-        // And check if it's the right one
-        if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]]) {
-     
-            // If it is, subscribe to it
-            [peripheral setNotifyValue:YES forCharacteristic:characteristic];
-        }
-    }
-    
-    // Once this is complete, we just need to wait for the data to come in.
-}
-
-
-/** This callback lets us know more data has arrived via notification on the characteristic
- */
-- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
-{
-    if (error) {
-        NSLog(@"Error discovering characteristics: %@", [error localizedDescription]);
-        return;
-    }
+- (void) isConnected {
     [[self connected] setOn: true];
-
-    
-    NSString *stringFromData = [[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding];
-    
-    // Have we got everything we need?
-    if ([stringFromData isEqualToString:@"EOM"]) {
-        
-        // We have, so show the data, 
-        [self.textview setText:[[NSString alloc] initWithData:self.data encoding:NSUTF8StringEncoding]];
-        
-        // Cancel our subscription to the characteristic
-        [peripheral setNotifyValue:NO forCharacteristic:characteristic];
-        
-        // and disconnect from the peripehral
-        [self.centralManager cancelPeripheralConnection:peripheral];
-    }
-
-    // Otherwise, just add the data on to what we already have
-    [self.data appendData:characteristic.value];
-    
-    // Log it
-    //NSLog(@"Received: %@", stringFromData);
 }
-
-
-/** The peripheral letting us know whether our subscribe/unsubscribe happened or not
- */
-- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
-{
-    if (error) {
-        NSLog(@"Error changing notification state: %@", error.localizedDescription);
-    }
-    
-    // Exit if it's not the transfer characteristic
-    if (![characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]]) {
-        return;
-    }
-    
-    // Notification has started
-    if (characteristic.isNotifying) {
-        NSLog(@"Notification began on %@", characteristic);
-    }
-    
-    // Notification has stopped
-    else {
-        // so disconnect from the peripheral
-        NSLog(@"Notification stopped on %@.  Disconnecting", characteristic);
-        [self.centralManager cancelPeripheralConnection:peripheral];
-    }
-}
-
-
-/** Once the disconnection happens, we need to clean up our local copy of the peripheral
- */
-- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
-{
-    NSLog(@"Peripheral Disconnected");
-    self.discoveredPeripheral = nil;
+- (void) isDisconnected {
     [[self connected] setOn: false];
-
-    
-    // We're disconnected, so start scanning again
-    [self scan];
-}
-
-
-/** Call this when things either go wrong, or you're done with the connection.
- *  This cancels any subscriptions if there are any, or straight disconnects if not.
- *  (didUpdateNotificationStateForCharacteristic will cancel the connection if a subscription is involved)
- */
-- (void)cleanup
-{
-    // Don't do anything if we're not connected
-    if (!self.discoveredPeripheral.isConnected) {
-        return;
-    }
-    
-    // See if we are subscribed to a characteristic on the peripheral
-    if (self.discoveredPeripheral.services != nil) {
-        for (CBService *service in self.discoveredPeripheral.services) {
-            if (service.characteristics != nil) {
-                for (CBCharacteristic *characteristic in service.characteristics) {
-                    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]]) {
-                        if (characteristic.isNotifying) {
-                            // It is notifying, so unsubscribe
-                            [self.discoveredPeripheral setNotifyValue:NO forCharacteristic:characteristic];
-                            
-                            // And we're done.
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    // If we've got this far, we're connected, but we're not subscribed, so we just disconnect
-    [self.centralManager cancelPeripheralConnection:self.discoveredPeripheral];
 }
 
 
--- BTLE Transfer/BTLEPeripheralViewController.m
+++ BTLE Transfer/BTLEPeripheralViewController.m
@@ -53,20 +53,16 @@
 #import "BTLEPeripheralViewController.h"
 #import <CoreBluetooth/CoreBluetooth.h>
 #import "TransferService.h"
+#import "BTLEPeripheral.h"
 
-
-@interface BTLEPeripheralViewController () <CBPeripheralManagerDelegate, UITextViewDelegate>
+@interface BTLEPeripheralViewController () <UITextViewDelegate, SenderDelegate>
 @property (strong, nonatomic) IBOutlet UITextView       *textView;
 @property (strong, nonatomic) IBOutlet UISwitch         *advertisingSwitch;
 
 
-@property (strong, nonatomic) CBPeripheralManager       *peripheralManager;
-@property (strong, nonatomic) CBMutableCharacteristic   *transferCharacteristic;
-@property (strong, nonatomic) NSData                    *dataToSend;
-@property (nonatomic, readwrite) NSInteger              sendDataIndex;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *channel;
 @property (weak, nonatomic) IBOutlet UILabel *channelText;
-
+@property (strong, nonatomic) BTLEPeripheral *peripheral;
 
 - (IBAction)channelChanged:(UISegmentedControl *)sender;
 
@@ -90,29 +86,17 @@ CBMutableService *transferService;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self startBluetooth];
+    self.peripheral = [[BTLEPeripheral alloc] initWith: self];
+    [self.peripheral startBluetooth];
+    
     [self channelChangedToInteger: 0];
 }
 
 
 - (void)viewWillDisappear:(BOOL)animated
 {
-    [self stopBluetooth];
+    [self.peripheral stopBluetooth];
     [super viewWillDisappear:animated];
-}
-
-- (void) startBluetooth {
-    // Start up the CBPeripheralManager
-    _peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil];
-}
-
-- (void) stopBluetooth {
-    // Don't keep it going while we're not showing.
-    [self.peripheralManager stopAdvertising];
-    if (transferService) {
-        [self.peripheralManager removeService: transferService];
-        transferService = nil;
-    }
 }
 
 
@@ -121,186 +105,16 @@ CBMutableService *transferService;
     
     [self channelChangedToInteger: sender.selectedSegmentIndex];
 }
+
 - (IBAction)channelChangedToInteger:(NSInteger *)sender {
-    
     [[self advertisingSwitch] setOn: false];
     [self switchChanged: nil];
     [TransferService setValue: sender];
     self.channelText.text = [TransferService getTRANSFER_SERVICE_UUID];
-
 }
-
-
-
-#pragma mark - Peripheral Methods
-
-
-
-/** Required protocol method.  A full app should take care of all the possible states,
- *  but we're just waiting for  to know when the CBPeripheralManager is ready
- */
-- (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral
-{
-    // Opt out from any other state
-    if (peripheral.state != CBPeripheralManagerStatePoweredOn) {
-        NSLog(@"self.peripheralManager state change to %ld", (long)peripheral.state);
-        return;
-    }
-    
-    // We're in CBPeripheralManagerStatePoweredOn state...
-    NSLog(@"self.peripheralManager powered on.");
-    
-    // ... so build our service.
-    
-    // Start with the CBMutableCharacteristic
-    self.transferCharacteristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]
-                                                                      properties:CBCharacteristicPropertyNotify
-                                                                           value:nil
-                                                                     permissions:CBAttributePermissionsReadable];
-
-    // Then the service
-    transferService = [[CBMutableService alloc] initWithType:[TransferService getTRANSFER_SERVICE_CBUUID]
-                                                                        primary:YES];
-    
-    // Add the characteristic to the service
-    transferService.characteristics = @[self.transferCharacteristic];
-    
-    // And add it to the peripheral manager
-    [self.peripheralManager addService:transferService];
-    
-    [self.peripheralManager startAdvertising:@{ CBAdvertisementDataServiceUUIDsKey : @[[TransferService getTRANSFER_SERVICE_CBUUID]] }];
-
-}
-
-
-/** Catch when someone subscribes to our characteristic, then start sending them data
- */
-- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic
-{
-    NSLog(@"Central subscribed to characteristic");
-    
-    // Get the data
-    self.dataToSend = [self.textView.text dataUsingEncoding:NSUTF8StringEncoding];
-    
-    // Reset the index
-    self.sendDataIndex = 0;
-    
-    // Start sending
-    [self sendData];
-}
-
-
-/** Recognise when the central unsubscribes
- */
-- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didUnsubscribeFromCharacteristic:(CBCharacteristic *)characteristic
-{
-    NSLog(@"Central unsubscribed from characteristic");
-}
-
-
-/** Sends the next amount of data to the connected central
- */
-- (void)sendData
-{
-    // First up, check if we're meant to be sending an EOM
-    static BOOL sendingEOM = NO;
-    
-    if (sendingEOM) {
-        
-        // send it
-        BOOL didSend = [self.peripheralManager updateValue:[@"EOM" dataUsingEncoding:NSUTF8StringEncoding] forCharacteristic:self.transferCharacteristic onSubscribedCentrals:nil];
-        
-        // Did it send?
-        if (didSend) {
-            
-            // It did, so mark it as sent
-            sendingEOM = NO;
-            
-            NSLog(@"Sent: EOM");
-        }
-        
-        // It didn't send, so we'll exit and wait for peripheralManagerIsReadyToUpdateSubscribers to call sendData again
-        return;
-    }
-    
-    // We're not sending an EOM, so we're sending data
-    
-    // Is there any left to send?
-    
-    if (self.sendDataIndex >= self.dataToSend.length) {
-        
-        // No data left.  Do nothing
-        return;
-    }
-    
-    // There's data left, so send until the callback fails, or we're done.
-    
-    BOOL didSend = YES;
-    
-    while (didSend) {
-        
-        // Make the next chunk
-        
-        // Work out how big it should be
-        NSInteger amountToSend = self.dataToSend.length - self.sendDataIndex;
-        
-        // Can't be longer than 20 bytes
-        if (amountToSend > NOTIFY_MTU) amountToSend = NOTIFY_MTU;
-        
-        // Copy out the data we want
-        NSData *chunk = [NSData dataWithBytes:self.dataToSend.bytes+self.sendDataIndex length:amountToSend];
-        
-        // Send it
-        didSend = [self.peripheralManager updateValue:chunk forCharacteristic:self.transferCharacteristic onSubscribedCentrals:nil];
-        
-        // If it didn't work, drop out and wait for the callback
-        if (!didSend) {
-            return;
-        }
-        
-        NSString *stringFromData = [[NSString alloc] initWithData:chunk encoding:NSUTF8StringEncoding];
-        NSLog(@"Sent: %@", stringFromData);
-        
-        // It did send, so update our index
-        self.sendDataIndex += amountToSend;
-        
-        // Was it the last one?
-        if (self.sendDataIndex >= self.dataToSend.length) {
-            
-            // It was - send an EOM
-            
-            // Set this so if the send fails, we'll send it next time
-            sendingEOM = YES;
-            
-            // Send it
-            BOOL eomSent = [self.peripheralManager updateValue:[@"EOM" dataUsingEncoding:NSUTF8StringEncoding] forCharacteristic:self.transferCharacteristic onSubscribedCentrals:nil];
-            
-            if (eomSent) {
-                // It sent, we're all done
-                sendingEOM = NO;
-                
-                NSLog(@"Sent: EOM");
-            }
-            
-            return;
-        }
-    }
-}
-
-
-/** This callback comes in when the PeripheralManager is ready to send the next chunk of data.
- *  This is to ensure that packets will arrive in the order they are sent
- */
-- (void)peripheralManagerIsReadyToUpdateSubscribers:(CBPeripheralManager *)peripheral
-{
-    // Start sending again
-    [self sendData];
-}
-
 
 
 #pragma mark - TextView Methods
-
 
 
 /** This is called when a change happens, so we know to stop advertising
@@ -343,13 +157,37 @@ CBMutableService *transferService;
 - (IBAction)switchChanged:(id)sender
 {
     if (self.advertisingSwitch.on) {
-        [self startBluetooth];
+        [self.peripheral startBluetooth];
     }
     
     else {
-        [self stopBluetooth];
+        [self.peripheral stopBluetooth];
     }
 }
 
+
+#pragma mark - SenderDelegate Methods
+
+- (NSData *)dataToSend {
+    NSString *dataString = self.textView.text;
+    if (false) {
+        NSLog(@"send %@", dataString);
+        return [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    }
+    if (dataString) {
+        NSString *searchKeyword = @"\n";
+        NSRange rangeOfYourString = [dataString rangeOfString: searchKeyword];
+        if(rangeOfYourString.location == NSNotFound) {
+            return nil;
+        } else{
+            NSString *toSendString = [dataString substringToIndex:rangeOfYourString.location];
+            NSString *finalString = [dataString substringFromIndex:rangeOfYourString.location+1];
+            [self.textView setText: finalString];
+            NSLog(@"send %@", toSendString);
+            return [toSendString dataUsingEncoding:NSUTF8StringEncoding];
+        }
+    }
+    return nil;
+}
 
 @end
